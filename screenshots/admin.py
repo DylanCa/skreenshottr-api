fe148acace28b12409dadd8a5ea991d70d99e80b
DylanCa <dylan.cattelan@gmail.com>
2023-07-09 18:40:15 +0200

    Added Tag to admin view

--- screenshots/admin.py
+++ screenshots/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
-from .models import User, Screenshot
+from .models import User, Screenshot, Tag
 
-admin.site.register([User, Screenshot])
+admin.site.register([User, Screenshot, Tag])
