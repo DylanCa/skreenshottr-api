ab29ec457cbef5825fe8b315bf4a404b35a64d2e
DylanCa <dylan.cattelan@gmail.com>
2023-07-09 18:43:52 +0200

    Imported TagQuery and TagMutation in schema

--- screenshots/schemas/schema.py
+++ screenshots/schemas/schema.py
@@ -3,16 +3,19 @@ import graphene
 from .authentication import AuthMutation
 from .user import UserQuery, UserMutation
 from .screenshot import ScreenshotQuery, ScreenshotMutation
+from .tag import TagQuery, TagMutation
 
 
 class Query(UserQuery,
             ScreenshotQuery,
+            TagQuery,
             graphene.ObjectType):
     pass
 
 
 class Mutation(UserMutation,
                ScreenshotMutation,
+               TagMutation,
                AuthMutation,
                graphene.ObjectType):
     pass
