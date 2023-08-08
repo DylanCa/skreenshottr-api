49d28ceb487123d47ce3092729a5b863f2bda80c
DylanCa <dylan.cattelan@gmail.com>
2023-08-08 12:08:45 +0200

    Renamed save_image_to_storage to upload_to_storage in ImageHelper

--- screenshots/lib/image_helper.py
+++ screenshots/lib/image_helper.py
@@ -1,6 +1,5 @@
 import logging
 
-from PIL import Image
 from screenshots.lib.file_manager import FileManager
 
 logger = logging.getLogger(__name__)
@@ -9,18 +8,7 @@ logger = logging.getLogger(__name__)
 class ImageHelper:
 
     @staticmethod
-    def verify_image_from_file(file) -> bool:
-        try:
-
-            img = Image.open(file)
-            img.verify()
-            return True
-        except Exception as e:
-            logger.exception(e)
-            return False
-
-    @staticmethod
-    def save_image_in_storage(file, directory):
+    def upload_to_storage(file, directory):
         fm = FileManager()
         url = fm.upload_to_s3_and_retrieve_url(file, directory)
 
