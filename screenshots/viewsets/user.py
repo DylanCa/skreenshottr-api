f942523f287d0e623107cab983787f7626c16ae4
DylanCa <dylan.cattelan@gmail.com>
2023-08-08 15:03:17 +0200

    Customized UserViewSet

--- screenshots/viewsets/user.py
+++ screenshots/viewsets/user.py
@@ -1,9 +1,37 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.viewsets import mixins
 
 from screenshots.models import User
-from screenshots.serializers import UserSerializer
+from screenshots.serializers import UserSerializer, ChangePasswordSerializer
+from screenshots.viewsets.mixins import BaseModelViewSetMixin
+from screenshots.viewsets.permissions import IsOwner
 
 
-class UserViewSet(viewsets.ReadOnlyModelViewSet):
+class UserViewSet(viewsets.GenericViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated,
+                          IsOwner]
+
+    def get_object(self) -> User:
+        return self.request.user
+
+    @action(methods=['GET', 'PATCH'], detail=False)
+    def me(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        if self.request.method == 'PATCH':
+            serializer = self.get_serializer(instance,
+                                             data=self.request.data,
+                                             partial=True)
+            if serializer.is_valid(raise_exception=True):
+                instance = serializer.save()
+
+        data = self.get_serializer(instance=instance).data
+        return Response(data=data)
+
+
+
+
