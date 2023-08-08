ea1d477ed356b4d6cb9e0c2712ee3fd1c8597627
DylanCa <dylan.cattelan@gmail.com>
2023-08-08 12:16:35 +0200

    Added custom validate in ScreenshotSerializer

--- screenshots/serializers/screenshot.py
+++ screenshots/serializers/screenshot.py
@@ -18,3 +18,19 @@ class ScreenshotSerializer(BaseModelSerializerMixin):
         model = Screenshot
         fields = ["id", "name", "file", "file_url", "tags", "owner_id"]
         read_only_fields = ["file_url"]
+
+    def validate(self, attrs):
+        attrs = self.validate_owner(attrs)
+        user = attrs['owner']
+
+        if "file" in attrs:
+            file = attrs.pop('file')
+            file_url = ImageHelper.upload_to_storage(file,
+                                                     user.id)
+            attrs['file_url'] = file_url
+
+            if "name" not in attrs:
+                filename = os.path.splitext(file.name)[0]
+                attrs['name'] = filename
+
+        return attrs
