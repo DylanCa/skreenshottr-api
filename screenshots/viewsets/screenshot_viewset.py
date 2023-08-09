8484100c256d856596b72506e311b1a73dbfd209
DylanCa <dylan.cattelan@gmail.com>
2023-08-09 17:18:22 +0200

    Removed PUT method from ScreenshotViewSet

--- screenshots/viewsets/screenshot_viewset.py
+++ screenshots/viewsets/screenshot_viewset.py
@@ -1,11 +1,15 @@
 from screenshots.models import Screenshot
 from screenshots.serializers import ScreenshotSerializer
 from .mixins import BaseModelViewSetMixin
+from rest_framework import viewsets, permissions
+
+from .permissions import IsOwner
 
 
 class ScreenshotViewSet(BaseModelViewSetMixin):
     queryset = Screenshot.objects.all()
     serializer_class = ScreenshotSerializer
+    http_method_names = ['get', 'post', 'patch', 'delete']
 
     def get_queryset(self):
         return Screenshot.objects.filter(owner=self.request.user).order_by('id')
