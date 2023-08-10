ec5813390336598c2c179edec40f28da35cbb271
DylanCa <dylan.cattelan@gmail.com>
2023-08-10 20:42:00 +0200

    Moved owner to BaseModelMixin

--- screenshots/models/tag.py
+++ screenshots/models/tag.py
@@ -1,12 +1,10 @@
 from django.db import models
 
-from .user import User
 from .mixins import BaseModelMixin
 
 
 class Tag(BaseModelMixin):
     name = models.CharField(max_length=256)
-    owner = models.ForeignKey(User, on_delete=models.RESTRICT, default=None)
 
     class Meta:
         db_table = "tags"
