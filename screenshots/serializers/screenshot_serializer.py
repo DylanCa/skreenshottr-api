dde911e1174cab84cab33f0a8cc651334d7ceb2c
DylanCa <dylan.cattelan@gmail.com>
2023-08-11 16:39:02 +0200

    Added new fields to ScreenshotSerializer

--- screenshots/serializers/screenshot_serializer.py
+++ screenshots/serializers/screenshot_serializer.py
@@ -1,5 +1,6 @@
 import os
 
+from PIL import Image
 from rest_framework import serializers
 
 from screenshots.lib import ImageHelper
@@ -16,8 +17,8 @@ class ScreenshotSerializer(BaseModelSerializerMixin):
 
     class Meta:
         model = Screenshot
-        fields = ["id", "name", "file", "file_url", "tags", "owner_id"]
-        read_only_fields = ["file_url"]
+        fields = ["id", "name", "description", "file", "filename", "image_url", "thumbnail_url", "format", "size", "width", "height", "tags", "owner_id"]
+        read_only_fields = [ "filename", "image_url", "thumbnail_url", "format", "size", "width", "height"]
 
     def validate(self, attrs):
         attrs = self.validate_owner(attrs)
@@ -25,9 +26,19 @@ class ScreenshotSerializer(BaseModelSerializerMixin):
 
         if "file" in attrs:
             file = attrs.pop('file')
-            file_url = ImageHelper.upload_to_storage(file,
-                                                     user.id)
-            attrs['file_url'] = file_url
+            filename = file.name
+
+            with Image.open(file) as image:
+                attrs['filename'] = filename
+                attrs['format'] = image.format
+                attrs['width'] = image.width
+                attrs['height'] = image.height
+                attrs['size'] = file.size
+
+                image_url, thumbnail_url = ImageHelper.upload_image_to_storage(image, filename, user.id)
+
+                attrs['image_url'] = image_url
+                attrs['thumbnail_url'] = thumbnail_url
 
             if "name" not in attrs:
                 filename = os.path.splitext(file.name)[0]
