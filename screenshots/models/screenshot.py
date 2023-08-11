a9221feb1d7d698099010b3648a424be847e6acc
DylanCa <dylan.cattelan@gmail.com>
2023-08-11 16:38:30 +0200

    Added description, thumbnail_url, size, width, height, format to Screenshot

--- screenshots/models/screenshot.py
+++ screenshots/models/screenshot.py
@@ -6,8 +6,16 @@ from .mixins import BaseModelMixin
 
 class Screenshot(BaseModelMixin):
     name = models.CharField(max_length=128)
-    file_url = models.CharField(max_length=256)
+    description = models.TextField(max_length=256, null=True)
+    image_url = models.URLField(max_length=256)
+    thumbnail_url = models.URLField(max_length=256)
+    filename = models.CharField(max_length=128)
     tags = models.ManyToManyField(Tag, default=None)
+
+    format = models.CharField(max_length=8)
+    size = models.IntegerField()
+    width = models.IntegerField()
+    height = models.IntegerField()
 
     class Meta:
         db_table = "screenshots"
