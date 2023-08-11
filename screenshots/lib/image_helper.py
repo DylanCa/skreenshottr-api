5cf3009a675cb57b5c76737f2d08d764bc39e33b
DylanCa <dylan.cattelan@gmail.com>
2023-08-11 16:39:19 +0200

    Modified FileManager and ImageHelper to handle thumbnails

--- screenshots/lib/image_helper.py
+++ screenshots/lib/image_helper.py
@@ -1,4 +1,7 @@
 import logging
+from io import BytesIO
+
+from PIL import Image
 
 from screenshots.lib.file_manager import FileManager
 
@@ -8,8 +11,31 @@ logger = logging.getLogger(__name__)
 class ImageHelper:
 
     @staticmethod
-    def upload_to_storage(file, directory):
-        fm = FileManager()
-        url = fm.upload_to_s3_and_retrieve_url(file, directory)
+    def upload_image_to_storage(image, filename, directory):
+        thumb_url = ImageHelper.__generate_and_upload_thumbnail_to_storage(image, filename, directory)
+        file_url = ImageHelper.__generate_and_upload_image_to_storage(image, filename, directory)
 
+        return file_url, thumb_url
+
+    @staticmethod
+    def __generate_and_upload_thumbnail_to_storage(thumb, filename, directory):
+        max_size = (200, 200)
+        thumb.thumbnail(size=max_size)
+        file = BytesIO()
+        thumb.save(file, thumb.format)
+
+        directory = f'{directory}/thumbnails'
+        filename = f"thumb_{filename}"
+        return ImageHelper.__upload(file, filename, directory)
+
+    @staticmethod
+    def __generate_and_upload_image_to_storage(image, filename, directory):
+        file = BytesIO()
+        image.save(file, image.format)
+        return ImageHelper.__upload(file, filename, directory)
+
+    @staticmethod
+    def __upload(file, filename, directory):
+        fm = FileManager()
+        url = fm.upload_to_s3_and_retrieve_url(file, filename, directory)
         return url
