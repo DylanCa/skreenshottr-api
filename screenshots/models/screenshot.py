8c45d50d410c92209ebea50ffd42d6e0f6abfb57
DylanCa <dylan.cattelan@gmail.com>
2023-08-13 18:03:04 +0200

    Removed owner from BaseModelMixin and added it in Screenshot & Tags models

--- screenshots/models/screenshot.py
+++ screenshots/models/screenshot.py
@@ -1,6 +1,8 @@
 from django.db import models
 
+from . import User
 from .tag import Tag
+from .application import Application
 from .mixins import BaseModelMixin
 
 
@@ -11,11 +13,14 @@ class Screenshot(BaseModelMixin):
     thumbnail_url = models.URLField(max_length=256)
     filename = models.CharField(max_length=128)
     tags = models.ManyToManyField(Tag, default=None)
+    application = models.ForeignKey(Application, on_delete=models.SET_NULL, null=True)
 
     format = models.CharField(max_length=8)
     size = models.IntegerField()
     width = models.IntegerField()
     height = models.IntegerField()
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
 
     class Meta:
         db_table = "screenshots"
