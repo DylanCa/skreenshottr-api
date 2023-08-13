8f799124471f548844fa9a456691d541a3425475
DylanCa <dylan.cattelan@gmail.com>
2023-08-13 18:03:16 +0200

    Created ApplicationSerializer

---
+++ screenshots/serializers/application_serializer.py
@@ -0,0 +1,15 @@
+from django.core.exceptions import ValidationError
+from rest_framework import serializers
+
+from screenshots.models import Application
+from screenshots.serializers.mixins import BaseModelSerializerMixin
+
+
+class ApplicationSerializer(BaseModelSerializerMixin):
+    class Meta:
+        model = Application
+        fields = ['id', 'name']
+        extra_kwargs = {
+            'name': {'validators': [],
+                     "allow_blank": True},
+        }
