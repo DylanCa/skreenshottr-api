13bb1bba90431b77dd2a2c9d9f47538a880f9d75
DylanCa <dylan.cattelan@gmail.com>
2023-08-18 14:55:28 +0200

    Added owner to application

--- screenshots/models/application.py
+++ screenshots/models/application.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+from . import User
 from .mixins import BaseModelMixin
 
 
 class Application(BaseModelMixin):
     name = models.CharField(max_length=256, unique=True, null=False)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
 
     class Meta:
         db_table = "applications"
